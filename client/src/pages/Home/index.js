c01df3800fdaa29a963dbdb1a90d3e48de359266
Betul Erkoc <betulfatmaerkoc@gmail.com>
2022-01-30 16:54:32 +0300

    implement subscription and fragment for post

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,27 @@
+import { useEffect } from "react";
 import { ListGroup } from "react-bootstrap";
 import Post from "../../components/Post";
 import { useQuery } from "@apollo/client";
-import {GET_ALL_POSTS} from "./queries";
+import {GET_ALL_POSTS, POST_SUBSCRIPTION} from "./queries";
 
 function Home() {
-  const { loading, error, data } = useQuery(GET_ALL_POSTS);
+  const { loading, error, data, subscribeToMore } = useQuery(GET_ALL_POSTS);
+
+  useEffect(() => {
+    subscribeToMore({
+      document: POST_SUBSCRIPTION,
+      updateQuery: (prev, {subscriptionData}) => {
+        console.log(subscriptionData)
+        console.log(prev)
+        if(!subscriptionData.data) return prev;
+        return {
+          posts: [
+            ...prev.posts, subscriptionData.data.postCreated
+          ]
+        }
+      }
+    })
+  }, [subscribeToMore])
 
   if (loading) {
     return <h1>Loading</h1>
--- client/src/pages/Home/queries.js
+++ client/src/pages/Home/queries.js
@@ -1,15 +1,33 @@
 import { gql } from "@apollo/client";
 
+const postFragment  = gql`
+  fragment PostFragment on Post {
+    id
+    title
+    description
+    user{
+      image
+    }
+  }
+`;
+
 export const GET_ALL_POSTS = gql`
   query getAllPost {
     posts{
-      id
-      title
-      description
-        user{
-          image
-        }
+      ...PostFragment
     }
   }
-
+${postFragment}
 `;
+
+export const POST_SUBSCRIPTION = gql`
+  subscription {
+      postCreated{
+        ...PostFragment
+      }
+  }
+  ${postFragment}
+`;
+
+
+
